6dc37aaff032420fb1a5cf500b4eb894f4a88215
jo <josehehall98@gmail.com>
2024-06-11 22:16:10 -0600

    I can add topping and iceCream flavers to the dom

--- app.js
+++ app.js
@@ -3,34 +3,40 @@
 const iceCream = [{
     name: 'Cookie Dough',
     price: 1.25,
-    quantity: 0
+    quantity: 0,
+    total: 0
 },
 {
     name: 'Vanilla',
     price: 1,
-    quantity: 0
+    quantity: 0,
+    total: 0
 },
 {
     name: 'Strawberry',
     price: 1.25,
-    quantity: 0
+    quantity: 0,
+    total: 0
     // @ts-ignore
 }]
 
 const toppings = [{
     name: 'Sprinkles',
     price: .25,
-    quantity: 0
+    quantity: 0,
+    total: 0
 },
 {
     name: 'Chocolate Chips',
     price: .25,
-    quantity: 0
+    quantity: 0,
+    total: 0
 },
 {
     name: 'Cookie Chunks',
     price: .5,
-    quantity: 0
+    quantity: 0,
+    total: 0
 }]
 
 
@@ -41,31 +47,51 @@ function addToppers(topItem) {
 
         }
     }
-    //  drawOrder()
+    drawToppers()
+}
+
+function drawToppers() {
+
+    let orderTopping = ''
+    const oderList = document.getElementById('toppings');
+    for (let i = 0; i < toppings.length; i++) {
+        const item = toppings[i]
+        if (item.quantity > 0) {
+            orderTopping += `<p class="mb-1 fw-bold"><i class="mdi mdi-ice-cream"></i> ${item.name} x${item.quantity} $${(item.price * item.quantity).toFixed(2)}</p>`
+        }
+    }
+    oderList.innerHTML = orderTopping
 }
 function addIceCream(flavor) {
     for (let i = 0; i < iceCream.length; i++) {
         if (flavor == iceCream[i].name) {
             iceCream[i].quantity += 1;
         }
-
     }
-    drawOrder()
+    drawIceCream()
 }
 
 
-function drawOrder() {
+function drawIceCream() {
 
-    let orderContent = ''
-    const oder = document.getElementById('order');
-    const oderList = document.getElementById('');
-
+    let orderFlavors = ''
+    const oder = document.getElementById('flavors');
     for (let i = 0; i < iceCream.length; i++) {
-        const item = iceCream[i]
-        // console.log(`- ${item.name} x${item.quantity} $${item.price * item.quantity}`);
-        if (item.quantity > 0) {
-            orderContent += `<p class="mb-1 fw-bold"><i class="mdi mdi-ice-cream"></i> ${item.name} x${item.quantity} $${(item.price * item.quantity).toFixed(2)}</p>`
+        const iceCF = iceCream[i]
+        if (iceCF.quantity > 0) {
+            orderFlavors += `<p class="mb-1 fw-bold"><i class="mdi mdi-ice-cream"></i> ${iceCF.name} ${iceCF.quantity} $${(iceCF.price * iceCF.quantity).toFixed(2)}</p>`
         }
     }
-    oder.innerHTML = orderContent
+    oder.innerHTML = orderFlavors
+}
+
+function checkOut() {
+    for (let i = 0; i < iceCream.length; i++) {
+        const item = iceCream[i]
+        const itemss = toppings[i]
+        item.quantity = 0
+        itemss.quantity = 0
+    }
+    drawIceCream()
+    drawToppers()
 }
